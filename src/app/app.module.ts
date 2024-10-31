d986cf0adda86253dac6b0163b6a7a5676db4d65
dayanandaeswar <dayanandaeswar@gmail.com>
2024-10-31 19:28:22 +0530

    added cross field validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddressComponent } from './components/address/address.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddressComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/address/address.component.ts
@@ -0,0 +1,73 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+
+
+export interface PostCodeLength {
+  key: string
+  value: string
+}
+
+export interface Country {
+  code: string
+  name: string
+}
+
+
+@Component({
+  selector: 'app-address',
+  templateUrl: './address.component.html',
+  styleUrl: './address.component.scss'
+})
+export class AddressComponent implements OnInit {
+
+  private formBuilder = inject(FormBuilder);
+  profileForm!: FormGroup;
+  countries!: Country[];
+  postalCodeLenght!: PostCodeLength[];
+  ngOnInit(): void {
+    //You get this from API
+    this.countries = JSON.parse('[{"name":"United States","code":"US"},{"name":"India","code":"IN"}]');
+    //You get this from API
+    this.postalCodeLenght = JSON.parse('[{"value":5,"key":"US"},{"value":6,"key":"IN"}]');
+
+    this.profileForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      address: this.formBuilder.group({
+        country: ['', Validators.required],
+        postalCode: ['', [Validators.required]],
+      }, { validators: validatePostCodeLengh('country', 'postalCode', this.postalCodeLenght) }),
+    });
+
+
+  }
+
+  profileFormSubmit() {
+    if (this.profileForm.valid) {
+      alert('Data validation is successful');
+    } else {
+      console.log(this.profileForm.get('address.postalCode')?.getError('invalidLength'));
+      alert('Please fill all required details');
+    }
+  }
+
+
+}
+
+
+function validatePostCodeLengh(firstControl: string, matcherControl: string, postalCodeLength: PostCodeLength[]): ValidatorFn {
+
+
+  return (control: AbstractControl): ValidationErrors | null => {
+
+    const sourceValue = control.get(firstControl)?.value;
+    const targetValue = control.get(matcherControl)?.value;
+    if (sourceValue && targetValue) {
+      const matchedRecord = postalCodeLength.find(el => el.key === sourceValue);
+      if (matchedRecord?.value !== targetValue.length) {
+        control.get(matcherControl)?.setErrors({ invalidLength: "Post code length must be " + matchedRecord?.value });
+      }
+    }
+    return null
+  }
+}
